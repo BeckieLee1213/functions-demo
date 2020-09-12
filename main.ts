36258226cc307c493cb1a99c99c64c216c8f2eec
Beckie Fischer <25963373+beckiefischer73@users.noreply.github.com>
2020-09-12 17:15:14 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,106 @@
+function moveDuck () {
+    if (controller.left.isPressed()) {
+        duck.x += -5
+    } else if (controller.right.isPressed()) {
+        duck.x += 5
+    }
+}
+function throwApple () {
+    projectile = sprites.createProjectileFromSprite(img`
+        . . . . . . . e c 7 . . . . . . 
+        . . . . e e e c 7 7 e e . . . . 
+        . . c e e e e c 7 e 2 2 e e . . 
+        . c e e e e e c 6 e e 2 2 2 e . 
+        . c e e e 2 e c c 2 4 5 4 2 e . 
+        c e e e 2 2 2 2 2 2 4 5 5 2 2 e 
+        c e e 2 2 2 2 2 2 2 2 4 4 2 2 e 
+        c e e 2 2 2 2 2 2 2 2 2 2 2 2 e 
+        c e e 2 2 2 2 2 2 2 2 2 2 2 2 e 
+        c e e 2 2 2 2 2 2 2 2 2 2 2 2 e 
+        c e e 2 2 2 2 2 2 2 2 2 2 4 2 e 
+        . e e e 2 2 2 2 2 2 2 2 2 4 e . 
+        . 2 e e 2 2 2 2 2 2 2 2 4 2 e . 
+        . . 2 e e 2 2 2 2 2 4 4 2 e . . 
+        . . . 2 2 e e 4 4 4 2 e e . . . 
+        . . . . . 2 2 e e e e . . . . . 
+        `, duck, 50, 100)
+}
+function setVariables () {
+    info.setScore(0)
+    fishChange = 1
+    fishSpeed = 5
+}
+controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
+    throwApple()
+})
+function setScene () {
+    scene.setBackgroundColor(9)
+    duck = sprites.create(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . b 5 5 b . . . 
+        . . . . . . b b b b b b . . . . 
+        . . . . . b b 5 5 5 5 5 b . . . 
+        . b b b b b 5 5 5 5 5 5 5 b . . 
+        . b d 5 b 5 5 5 5 5 5 5 5 b . . 
+        . . b 5 5 b 5 d 1 f 5 d 4 f . . 
+        . . b d 5 5 b 1 f f 5 4 4 c . . 
+        b b d b 5 5 5 d f b 4 4 4 4 b . 
+        b d d c d 5 5 b 5 4 4 4 4 4 4 b 
+        c d d d c c b 5 5 5 5 5 5 5 b . 
+        c b d d d d d 5 5 5 5 5 5 5 b . 
+        . c d d d d d d 5 5 5 5 5 d b . 
+        . . c b d d d d d 5 5 5 b b . . 
+        . . . c c c c c c c c b b . . . 
+        `, SpriteKind.Player)
+    duck.setPosition(20, 35)
+    fish = sprites.create(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . c c c c . . . . 
+        . . . . . . c c d d d d c . . . 
+        . . . . . c c c c c c d c . . . 
+        . . . . c c 4 4 4 4 d c c . . . 
+        . . . c 4 d 4 4 4 4 4 1 c . c c 
+        . . c 4 4 4 1 4 4 4 4 d 1 c 4 c 
+        . c 4 4 4 4 1 4 4 4 4 4 1 c 4 c 
+        f 4 4 4 4 4 1 4 4 4 4 4 1 4 4 f 
+        f 4 4 4 f 4 1 c c 4 4 4 1 f 4 f 
+        f 4 4 4 4 4 1 4 4 f 4 4 d f 4 f 
+        . f 4 4 4 4 1 c 4 f 4 d f f f f 
+        . . f f 4 d 4 4 f f 4 c f c . . 
+        . . . . f f 4 4 4 4 c d b c . . 
+        . . . . . . f f f f d d d c . . 
+        . . . . . . . . . . c c c . . . 
+        `, SpriteKind.Enemy)
+    fish.setPosition(75, 90)
+    game.showLongText("", DialogLayout.Bottom)
+}
+function updateScore () {
+    info.setScore(info.score() + 1)
+    if (info.score() > 15) {
+        fishSpeed = 8
+    }
+}
+function moveFish () {
+    if (fish.x > scene.screenWidth()) {
+        fishChange = -1
+    } else if (fish.x < 0) {
+        fishChange = 1
+    }
+    fish.x += fishChange * fishSpeed
+}
+sprites.onOverlap(SpriteKind.Projectile, SpriteKind.Enemy, function (sprite, otherSprite) {
+    fish.startEffect(effects.bubbles, 500)
+    updateScore()
+})
+let fish: Sprite = null
+let fishSpeed = 0
+let fishChange = 0
+let projectile: Sprite = null
+let duck: Sprite = null
+setScene()
+setVariables()
+game.onUpdate(function () {
+    moveFish()
+    moveDuck()
+})
